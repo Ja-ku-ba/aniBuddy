004192e2319d340b626156b70fe9500ce99b96e0
Ubuntu <wulfrykpierozek@gmail.com>
2024-02-24 23:48:41 +0100

    Post model'

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("description"[:50], "added")
+
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # owner = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    description = models.CharField(max_length=1024)
+    content = models.TextField()
+    # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    added = models.DateTimeField(auto_now_add=False)
