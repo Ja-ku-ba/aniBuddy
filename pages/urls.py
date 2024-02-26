c9c1d0e41de988f814304a8c0b5f1a0efd495d92
Ubuntu <wulfrykpierozek@gmail.com>
2024-02-26 16:05:36 +0100

    more post styling

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("add/<str:page>", views.addPost, name='add')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def addPost(request, page):
+    return render(request, 'pages/add.html')
