c9f9e320493237455bfe5b6beb663b7df139dca3
Ubuntu <wulfrykpierozek@gmail.com>
2024-02-26 17:03:43 +0100

    Add post backend

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
 
 def addPost(request, page):
-    return render(request, 'pages/add.html')
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post.objects.create(
+                description = request.POST.get('description'),
+                content = request.POST.get('content'),
+                added = datetime.now()
+            )
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'pages/add.html', context)
