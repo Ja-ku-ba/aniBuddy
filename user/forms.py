834c5ab04d666d8ca983b30fd7ae0afe26f23b66
Ubuntu <wulfrykpierozek@gmail.com>
2024-03-01 15:59:40 +0100

    login

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import Form, ModelForm
 
 from .models import MyUserModel
 
@@ -7,3 +9,27 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = MyUserModel
         fields = ["email", "username", "password1", "password2"]
+
+
+# class UserLoginForm(Form):
+#     email = forms.EmailField(required=True)
+#     password1 = forms.CharField(max_length=65, widget=forms.PasswordInput)
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(max_length=63, required=False)
+    # class Meta:
+    #     model = MyUserModel
+    #     fields = ["email", "password"]
+
+    def clean(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password1")
+        user_obj = MyUserModel.objects.filter(email=email).first()
+        if not user_obj:
+            raise forms.ValidationError("Sprawdź czy email i hasło są poprawne")
+        else:
+            if not user_obj.check_password(password):
+                raise forms.ValidationError("Sprawdź czy email i hasło są poprawne")
+        return super(UserLoginForm, self).clean(*args, **kwargs)
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
-from .forms import UserRegistrationForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -16,5 +17,22 @@ def register(request):
             login(request, user)
             return redirect("home")
 
-    context = {"form": form}
+    context = {"form": form, "action": "register"}
+    return render(request, "user/loginRegister.html", context)
+
+
+def login_user(request):
+    form = UserLoginForm()
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Witaj @{user.get_username()}, ponownie!")
+                return redirect("/")
+
+    context = {"form": form, "action": "login"}
     return render(request, "user/loginRegister.html", context)
