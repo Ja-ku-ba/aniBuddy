15cee731e2d130cb93d7547ce216d7bffc0521c8
Ubuntu <wulfrykpierozek@gmail.com>
2024-03-04 13:13:18 +0100

    save before merge

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
-from . import views
-
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path("", views.home, name='home'),
-    path("add/<str:page>", views.add, name='add')
+    path("", views.home, name="home"),
+    path("add/<str:page>", views.add, name="add"),
+    path("post/<int:id>", views.post_page, name="post_Page"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,9 @@ from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, "pages/home.html")
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "pages/home.html", context)
 
 
 def add(request, page):
@@ -17,6 +19,7 @@ def add(request, page):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             new_post = Post.objects.create(
+                owner=request.user,
                 description=request.POST.get("description"),
                 content=request.POST.get("content"),
                 added=datetime.now(),
@@ -26,9 +29,9 @@ def add(request, page):
                 new_post.image = image
                 new_post.save()
             return redirect("home")
-        # else:
-        # var = dir(form.errors.items())
-        # print(var)
-        # print(form.errors.get_json_data()["__all__"][0]["message"])
     context = {"form": form}
     return render(request, "pages/add.html", context)
+
+
+def post_page(request, id):
+    return render(request, "pages/postView.html")
