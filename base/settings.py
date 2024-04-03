3041eb251e5206736bfc3ba7d16e5d8388887252
Ubuntu <wulfrykpierozek@gmail.com>
2024-04-03 21:00:00 +0200

    popups positioning + tags are changed the way it displays proper colllor

--- base/settings.py
+++ base/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib import messages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,6 +77,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "base.wsgi.application"
 
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
